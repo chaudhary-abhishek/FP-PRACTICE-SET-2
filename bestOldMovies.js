d91a3d3f85706197c8200d2bb5f130bdabfea773
Abhishek Choudhary <abhishekch115@gmail.com>
2023-04-12 23:54:32 +0530

    Question-12

---
+++ bestOldMovies.js
@@ -0,0 +1,17 @@
+const bollywoodMovies = [
+    { title: 'Sholay', director: 'Ramesh Sippy', year: 1975, rating: 8.2 },
+    { title: 'Amar Akbar Anthony', director: 'Manmohan Desai', year: 1977, rating: 7.6 },
+    { title: 'Namak Halaal', director: 'Prakash Mehra', year: 1982, rating: 7.4 },
+    { title: 'Mr. India', director: 'Shekhar Kapur', year: 1987, rating: 7.8 },
+    { title: 'Qayamat Se Qayamat Tak', director: 'Mansoor Khan', year: 1988, rating: 7.6 },
+    { title: 'Parinda', director: 'Vidhu Vinod Chopra', year: 1989, rating: 8.1 },
+    { title: 'Dil', director: 'Indra Kumar', year: 1990, rating: 7.8 }
+  ];
+
+const getBestMovie = movie =>movie.year<1990&&movie.rating>8;
+//console.log(getBestMovie({ title: 'Sholay', director: 'Ramesh Sippy', year: 1975, rating: 8.2 }))
+
+const filteredMovie = bollywoodMovies.filter(getBestMovie);
+
+const movieTitle = movie =>movie.title;
+console.log(filteredMovie.map(movieTitle));
